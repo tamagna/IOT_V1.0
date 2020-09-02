3bd49670787370717c3d7bfe6d2752fa2d7b3bce
tamagna <onlytamagna@gmail.com>
2020-09-02 20:46:05 +0530

    Trigger when value changes, add time

--- ESP_Firebase/ESP_Firebase.ino
+++ ESP_Firebase/ESP_Firebase.ino
@@ -29,6 +29,9 @@ const String dbName = "devices";
 const int deviceId = 1;
 float currentTemp = 0.0;
 float currentHumid = 0.0;
+float lastTemp = 200.0;
+float lastHumid = 200.0;
+int minuteCount = 0;
 
 char mqttBroker[]  = "industrial.api.ubidots.com";
 char payload[100];
@@ -127,6 +130,9 @@ void setup()
  
 void loop()
 {
+  if (minuteCount > 5)
+    minuteCount = 0;
+    
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
@@ -160,46 +166,53 @@ void loop()
     delay(200);
   }
 
-  String strTemp = String(currentTemp);
-  strTemp = strTemp.substring(0, strTemp.length()-1);
-  String strHumid = String(currentHumid);
-  strHumid = strHumid.substring(0, strHumid.length()-1);
+  if ((currentTemp != lastTemp) || (currentHumid != lastHumid) || (minuteCount>=5)) {
+    String strTemp = String(currentTemp);
+    strTemp = strTemp.substring(0, strTemp.length()-1);
+    String strHumid = String(currentHumid);
+    strHumid = strHumid.substring(0, strHumid.length()-1);
+    
+    json.set("/temp", strTemp);
+    Firebase.updateNode(firebaseData, "/"+dbName+"/"+String(deviceId), json);
+    json.set("/humid", String(strHumid));
+    Firebase.updateNode(firebaseData, "/"+dbName+"/"+String(deviceId), json);
+    Firebase.setTimestamp(firebaseData, "/"+dbName+"/"+String(deviceId)+"/updatedAt");
+    Serial.println("Sent to Firebase!");
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+    delay(200);
   
-  json.set("/temp", strTemp);
-  Firebase.updateNode(firebaseData, "/"+dbName+"/"+String(deviceId), json);
-  json.set("/humid", String(strHumid));
-  Firebase.updateNode(firebaseData, "/"+dbName+"/"+String(deviceId), json);
-  Serial.println("Sent to Firebase!");
-  digitalWrite(ledPin, HIGH);
-  delay(200);
-  digitalWrite(ledPin, LOW);
-  delay(200);
+    if (!client.connected()) {
+      reconnect();
+    }
+    sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
+    sprintf(payload, "%s", ""); // Cleans the payload
+    sprintf(payload, "{\"%s\":", VARIABLE_LABEL1); // Adds the variable label
+    float sensor = currentTemp;
+    dtostrf(sensor, 4, 2, str_sensor);
+    sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
+    Serial.println("Publishing data to Ubidots Cloud");
+    client.publish(topic, payload);
+    client.loop();
+    sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
+    sprintf(payload, "%s", ""); // Cleans the payload
+    sprintf(payload, "{\"%s\":", VARIABLE_LABEL2); // Adds the variable label
+    sensor = currentHumid;
+    dtostrf(sensor, 4, 2, str_sensor);
+    sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
+    Serial.println("Publishing data to Ubidots Cloud");
+    client.publish(topic, payload);
+    client.loop();
+    Serial.println("Sent to Udibots!");
+  
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
 
-  if (!client.connected()) {
-    reconnect();
+    lastTemp = currentTemp;
+    lastHumid = currentHumid;
   }
-  sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
-  sprintf(payload, "%s", ""); // Cleans the payload
-  sprintf(payload, "{\"%s\":", VARIABLE_LABEL1); // Adds the variable label
-  float sensor = currentTemp;
-  dtostrf(sensor, 4, 2, str_sensor);
-  sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
-  Serial.println("Publishing data to Ubidots Cloud");
-  client.publish(topic, payload);
-  client.loop();
-  sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
-  sprintf(payload, "%s", ""); // Cleans the payload
-  sprintf(payload, "{\"%s\":", VARIABLE_LABEL2); // Adds the variable label
-  sensor = currentHumid;
-  dtostrf(sensor, 4, 2, str_sensor);
-  sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
-  Serial.println("Publishing data to Ubidots Cloud");
-  client.publish(topic, payload);
-  client.loop();
-  Serial.println("Sent to Udibots!");
-  
-  digitalWrite(ledPin, HIGH);
-  delay(200);
-  digitalWrite(ledPin, LOW);
-  delay(30000);
+  minuteCount++;
+  delay(60000);
 }
