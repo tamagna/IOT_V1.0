6443c94df0d0af6d32b32f6a1ea45420796a2aaf
tamagna <onlytamagna@gmail.com>
2020-09-07 21:47:20 +0530

    V2.0

--- ESP_Firebase/ESP_Firebase.ino
+++ ESP_Firebase/ESP_Firebase.ino
@@ -23,15 +23,28 @@
 #define VARIABLE_LABEL2 "humidity" // Assing the variable label
 #define DEVICE_LABEL "esp32" // Assig the device label
 
-const int ledPin = 21;
+// Pin configurations
 const int sensorPin = 4;
+const int ledTemp = 18;
+const int ledHumid = 19;
+const int ledWifi = 21;
+const int ledFirebase = 22;
+const int ledUdibots = 23;
+
 const String dbName = "devices";
 const int deviceId = 1;
-float currentTemp = 0.0;
-float currentHumid = 0.0;
+const int keepLedOn = 2000; // LEDs will be kept on for 2 seconds
+const int wifiReconnectDelay = 2 * 1000; // Retry reconneting Wifi after 2 seconds
+const int maxWifiReconnectBeforeRestart = 10; // ESP32 will be restarted if it fails to connect Wifi after 10 attempts
+const int sendInterval = 2 * 60 * 1000; // Read sensor every 2 minutes, if reading has changed then send
+const int unchangedCountThreshold = 10; // Send data forcibly after 10 interations, even if there are no changes in sensor data
+const int udibotReconnectDelay = 2 * 1000; // Retry reconneting Uditbot MQTT after 2 seconds
+const int maxUdibotReconnect = 10;
+
+int loopCounter = 0;
 float lastTemp = 200.0;
 float lastHumid = 200.0;
-int minuteCount = 0;
+unsigned long time_now = 0;
 
 char mqttBroker[]  = "industrial.api.ubidots.com";
 char payload[100];
@@ -56,9 +69,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println(topic);
 }
 
+// Reconnect to Udibots
 void reconnect() {
+  int count = 0;
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected() && count < maxUdibotReconnect) {
     Serial.println("Attempting MQTT connection...");
     
     // Attemp to connect
@@ -69,15 +84,104 @@ void reconnect() {
       Serial.print(client.state());
       Serial.println(" try again in 2 seconds");
       // Wait 2 seconds before retrying
-      delay(2000);
+      count++;
+      delay(udibotReconnectDelay);
     }
+  }
+  if(count >= maxUdibotReconnect)
+    Serial.println("Failed to connect, skipping.");
+}
+
+// Connect to Wifi
+boolean connectWifi(){
+  int wifiRetryCount = 0;
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED && wifiRetryCount < maxWifiReconnectBeforeRestart )
+  {
+    delay(wifiReconnectDelay);
+    Serial.print(".");
+    wifiRetryCount ++;
+  }
+  if (WiFi.status() != WL_CONNECTED){
+    Serial.println("Resetting");
+    ESP.restart();
+  }
+  else
+  {
+    Serial.println("");
+    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.println(WiFi.localIP());
+    return true;
+  }
+  return false;
+}
+
+// Send data to Udibots
+void sendToUdibots(float temp, float humid) {
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  if (client.connected()) {
+    sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
+    sprintf(payload, "%s", ""); // Cleans the payload
+    sprintf(payload, "{\"%s\":", VARIABLE_LABEL1); // Adds the variable label
+    float sensor = temp;
+    dtostrf(sensor, 4, 2, str_sensor);
+    sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
+    Serial.println("Publishing data to Ubidots Cloud");
+    client.publish(topic, payload);
+    client.loop();
+    sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
+    sprintf(payload, "%s", ""); // Cleans the payload
+    sprintf(payload, "{\"%s\":", VARIABLE_LABEL2); // Adds the variable label
+    sensor = humid;
+    dtostrf(sensor, 4, 2, str_sensor);
+    sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
+    Serial.println("Publishing data to Ubidots Cloud");
+    client.publish(topic, payload);
+    client.loop();
+    Serial.println("Sent to Udibots!");
+    digitalWrite(ledUdibots, HIGH);
+  }
+}
+
+// Send data to Firebase
+void sendToFirebase(float temp, float humid) {
+  float roundedTemp = (roundf(temp*10.))/10.;
+  String strTemp = String(roundedTemp);
+  strTemp = strTemp.substring(0, strTemp.length()-1);
+  
+  float roundedHumid = (roundf(humid*10.))/10.;
+  String strHumid = String(roundedHumid);
+  strHumid = strHumid.substring(0, strHumid.length()-1);
+  
+  json.set("/temp", strTemp);
+  Firebase.updateNode(firebaseData, "/"+dbName+"/"+String(deviceId), json);
+  json.set("/humid", String(strHumid));
+  Firebase.updateNode(firebaseData, "/"+dbName+"/"+String(deviceId), json);
+  Firebase.setTimestamp(firebaseData, "/"+dbName+"/"+String(deviceId)+"/updatedAt");
+  Serial.println("Sent to Firebase!");
+  digitalWrite(ledFirebase, HIGH);
+}
+
+void blinkLed(int ledPin, int duration, int count){
+  for (int i=0; i<count; i++){
+    digitalWrite(ledPin, HIGH);
+    delay(duration);
+    digitalWrite(ledPin, LOW);
+    delay(duration);
   }
 }
  
 void setup()
 { 
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+  pinMode(ledTemp, OUTPUT);
+  pinMode(ledHumid, OUTPUT);
+  pinMode(ledWifi, OUTPUT);
+  pinMode(ledFirebase, OUTPUT);
+  pinMode(ledUdibots, OUTPUT);
   dht.begin();
   // Print temperature sensor details.
   sensor_t sensor;
@@ -101,18 +205,9 @@ void setup()
   Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
   Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
   Serial.println(F("------------------------------------"));
-
+  
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
-  }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+  boolean wifiStatus = connectWifi();
  
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
@@ -130,89 +225,62 @@ void setup()
  
 void loop()
 {
-  if (minuteCount > 5)
-    minuteCount = 0;
-    
+  float temp;
+  float humid;
+  boolean tempWorking = false;
+  boolean humidWorking = false;
   sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
-    Serial.print(F("Temperature: "));
-    currentTemp = event.temperature;
-    currentTemp = (roundf(currentTemp*10.))/10.;
-    Serial.print(currentTemp);
-    Serial.println(F("°C"));
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-    delay(200);
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    Serial.print(F("Humidity: "));
-    currentHumid = event.relative_humidity;
-    currentHumid = (roundf(currentHumid*10.))/10.;
-    Serial.print(currentHumid);
-    Serial.println(F("%"));
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-    delay(200);
-  }
 
-  if ((currentTemp != lastTemp) || (currentHumid != lastHumid) || (minuteCount>=5)) {
-    String strTemp = String(currentTemp);
-    strTemp = strTemp.substring(0, strTemp.length()-1);
-    String strHumid = String(currentHumid);
-    strHumid = strHumid.substring(0, strHumid.length()-1);
-    
-    json.set("/temp", strTemp);
-    Firebase.updateNode(firebaseData, "/"+dbName+"/"+String(deviceId), json);
-    json.set("/humid", String(strHumid));
-    Firebase.updateNode(firebaseData, "/"+dbName+"/"+String(deviceId), json);
-    Firebase.setTimestamp(firebaseData, "/"+dbName+"/"+String(deviceId)+"/updatedAt");
-    Serial.println("Sent to Firebase!");
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
-    delay(200);
-  
-    if (!client.connected()) {
-      reconnect();
+  if ((time_now == 0) || (millis() > time_now + sendInterval)) {
+    time_now = millis();
+
+    blinkLed(ledTemp, 200, 3);
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      Serial.println(F("Error reading temperature!"));
     }
-    sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
-    sprintf(payload, "%s", ""); // Cleans the payload
-    sprintf(payload, "{\"%s\":", VARIABLE_LABEL1); // Adds the variable label
-    float sensor = currentTemp;
-    dtostrf(sensor, 4, 2, str_sensor);
-    sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
-    Serial.println("Publishing data to Ubidots Cloud");
-    client.publish(topic, payload);
-    client.loop();
-    sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
-    sprintf(payload, "%s", ""); // Cleans the payload
-    sprintf(payload, "{\"%s\":", VARIABLE_LABEL2); // Adds the variable label
-    sensor = currentHumid;
-    dtostrf(sensor, 4, 2, str_sensor);
-    sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
-    Serial.println("Publishing data to Ubidots Cloud");
-    client.publish(topic, payload);
-    client.loop();
-    Serial.println("Sent to Udibots!");
-  
-    digitalWrite(ledPin, HIGH);
-    delay(200);
-    digitalWrite(ledPin, LOW);
+    else {
+      Serial.print(F("Temperature: "));
+      temp = event.temperature;
+      Serial.print(temp);
+      Serial.println(F("°C"));
+      digitalWrite(ledTemp, HIGH);
+      tempWorking = true;
+    }
 
-    lastTemp = currentTemp;
-    lastHumid = currentHumid;
+    blinkLed(ledHumid, 200, 3);
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      Serial.println(F("Error reading humidity!"));
+    }
+    else {
+      Serial.print(F("Humidity: "));
+      humid = event.relative_humidity;
+      Serial.print(humid);
+      Serial.println(F("%"));
+      digitalWrite(ledHumid, HIGH);
+      humidWorking = true;
+    }
+
+    if ((temp != lastTemp && tempWorking == true) || (humid != lastHumid && humidWorking == true) || (loopCounter >= 5)) {
+      blinkLed(ledWifi, 200, 3);
+      boolean wifiStatus = connectWifi();    
+      if (wifiStatus)
+        digitalWrite(ledWifi, HIGH);
+      blinkLed(ledFirebase, 200, 3);
+      sendToFirebase(temp, humid);    
+      blinkLed(ledUdibots, 200, 3);
+      sendToUdibots(temp, humid);
+      lastTemp = temp;
+      lastHumid = humid;
+      delay(keepLedOn);
+      digitalWrite(ledTemp, LOW);
+      digitalWrite(ledHumid, LOW);
+      digitalWrite(ledWifi, LOW);
+      digitalWrite(ledFirebase, LOW);
+      digitalWrite(ledUdibots, LOW);
+      loopCounter = 0;
+    }
   }
-  minuteCount++;
-  delay(60000);
+  loopCounter++;
 }
